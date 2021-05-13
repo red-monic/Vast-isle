ec6bc8c3dd268f04e6cc92c7ed7165d22a7228b1
rufusnufus <nufusrufus@gmail.com>
2021-05-14 00:11:06 +0300

    refactor code

--- src/MyProject.hs
+++ src/MyProject.hs
@@ -51,9 +51,91 @@ example1 = do
     -- print $ "Second regular: " ++ show (nextGen 2 (fromGenotypes [geno1, geno2]))
     -- print $ "Second with pedofil: " ++ show (nextWithPedofil 2 Nothing (fromGenotypes [geno1, geno2]))
 
-next :: Generation -> Generation
-next = nextGen 1
--- Predefined traits and genotypes aka creatures
+--------------------------------------------------------------------------------
+-- |                   Data, newtypes and types declaration                    |
+--------------------------------------------------------------------------------
+
+-- TODO: donot like naming of Sort 
+data Sort = Dominant | Recessive
+    deriving (Show, Eq)
+
+newtype Allele = Allele {getSort :: Sort}
+    deriving (Eq)
+
+type Code = String
+
+data Trait = Trait {
+    representingCode :: Code, -- TODO: donot like this name too
+    getAlleles :: [Allele]
+}
+
+newtype Phenotype = Phenotype {getCodes :: [Code]}
+    deriving (Show)
+
+type Genotype = [Trait]
+
+-- | TODO: shit, floating!
+type ProbRatio = Float
+
+data Offspring = Offspring { getType :: Genotype, prob :: ProbRatio}
+    deriving (Eq)
+
+type Generation = [Offspring]
+
+--------------------------------------------------------------------------------
+--                                Class instances                              |
+--------------------------------------------------------------------------------
+
+-- | Shows |
+
+instance Show Allele where
+    show allele = "Allele: " ++ show (getSort allele)
+
+instance Show Trait where
+    show (Trait c [x, y]) = getAllelle c x ++ getAllelle c y
+    show (Trait _ _) = "Trait: empty"
+
+instance Show Offspring where
+    show (Offspring geno p) = "(Offspring: " ++ show geno ++ ", " ++ show p ++ ")"
+
+-- |     |
+
+instance Semigroup Allele where
+    Allele Dominant <> _ = Allele Dominant
+    _ <> Allele Dominant = Allele Dominant
+    _ <> _ = Allele Recessive
+
+instance Monoid Allele where
+    mempty = Allele Recessive
+
+-- | Ords |
+
+instance Ord Allele where
+    compare (Allele Recessive) (Allele Dominant) = GT
+    compare (Allele Dominant) (Allele Recessive) = LT
+    compare (Allele Dominant) (Allele Dominant) = EQ
+    compare (Allele Recessive) (Allele Recessive) = EQ
+
+instance Ord Trait where
+    compare (Trait c1 a1) (Trait c2 a2) = if c1 == c2
+                                        then compare (sort a1) (sort a2)
+                                        else compare (codeToLower c1) (codeToLower c2)
+
+instance Ord Offspring where
+    compare (Offspring g1 _) (Offspring g2 _) = compare (sort g1) (sort g2)
+
+
+-- | Eq |
+
+instance Eq Trait where
+    (Trait oneCode oneAlleles) == (Trait otherCode otherAlleles)
+        = sort oneAlleles == sort otherAlleles && oneCode == otherCode
+
+--------------------------------------------------------------------------------
+--                                   Examples                                  |
+--------------------------------------------------------------------------------
+
+-- |      Predefined traits and genotypes aka creatures       |
 
 
 -- example of fur trait, see code
@@ -85,18 +167,31 @@ catWithYellowFurAndBigEyes = [
     furTrait [recessiveAllele, recessiveAllele]
     ]
 
-
 firstGeneration :: Generation
 firstGeneration = fromGenotypes [catWithBlueFurAndBigEyes, catWithYellowFurAndBigEyes]
 
 -- end of Predefined traits and genotypes aka creatures
 
+geno1 :: Genotype
+geno1 = [eyeSizeTrait [recessiveAllele, dominantAllele], furTrait [recessiveAllele, dominantAllele]]
 
--- TODO: donot like naming of Sort 
-data Sort = Dominant | Recessive
-    deriving (Show, Eq)
+geno2 :: Genotype
+geno2 = [eyeSizeTrait [dominantAllele, dominantAllele], furTrait [dominantAllele, dominantAllele]]
 
-type Code = String
+geno3 :: Genotype
+geno3 = [eyeSizeTrait [recessiveAllele, dominantAllele], furTrait [dominantAllele, dominantAllele]]
+
+geno4 :: Genotype
+geno4 = [eyeSizeTrait [dominantAllele, recessiveAllele], furTrait [dominantAllele,recessiveAllele]]
+
+osp3 = Offspring [furTrait [dominantAllele, dominantAllele], eyeSizeTrait [recessiveAllele, recessiveAllele]] (1/3)
+osp4 = Offspring [furTrait [recessiveAllele, dominantAllele], eyeSizeTrait [recessiveAllele, dominantAllele]] (2/3)
+
+
+--------------------------------------------------------------------------------
+--                      Computations and Helper functions                      |
+--------------------------------------------------------------------------------
+
 
 -- TODO: shitnaming in these 2 function
 codeToUpper :: Code -> Code
@@ -105,67 +200,39 @@ codeToUpper = map toUpper
 codeToLower :: Code -> Code
 codeToLower = map toLower
 
-
-newtype Allele = Allele {getSort :: Sort}
-    deriving (Eq)
-
-instance Show Allele where
-    show allele = "Allele: " ++ show (getSort allele)
-
 dominantAllele :: Allele
 dominantAllele = Allele Dominant
 
 recessiveAllele :: Allele
 recessiveAllele = Allele Recessive
 
-instance Semigroup Allele where
-    Allele Dominant <> _ = Allele Dominant
-    _ <> Allele Dominant = Allele Dominant
-    _ <> _ = Allele Recessive
-
-instance Monoid Allele where
-    mempty = Allele Recessive
-
-instance Ord Allele where
-    compare (Allele Recessive) (Allele Dominant) = GT
-    compare (Allele Dominant) (Allele Recessive) = LT
-    compare (Allele Dominant) (Allele Dominant) = EQ
-    compare (Allele Recessive) (Allele Recessive) = EQ
-
-data Trait = Trait {
-    representingCode :: Code, -- TODO: donot like this name too
-    getAlleles :: [Allele]
-}
-
-instance Show Trait where
-    show (Trait c [x, y]) = getAllelle c x ++ getAllelle c y
-    show (Trait _ _) = "Trait: empty"
-    
-
-getAllelle :: Code -> Allele -> Code
-getAllelle code trait
+getCode :: Code -> Allele -> Code
+getCode code trait
     | trait == Allele Dominant = codeToUpper code
     | otherwise = codeToLower code
-
-
-instance Ord Trait where
-    compare (Trait c1 a1) (Trait c2 a2) = if c1 == c2
-                                        then compare (sort a1) (sort a2)
-                                        else compare (codeToLower c1) (codeToLower c2)
-
-instance Eq Trait where
-    (Trait oneCode oneAlleles) == (Trait otherCode otherAlleles)
-        = sort oneAlleles == sort otherAlleles && oneCode == otherCode
-
--- | TODO: shit, floating!
-type ProbRatio = Float
-
 
 -- sumUpper :: [(a, a), Int] -> (b, ProbRatio)
 
 listToElemWithLength :: [a] -> [(a, Int)]
 listToElemWithLength [] = []
 listToElemWithLength (x:xs) = [(x, length xs + 1)]
+
+toCode :: Trait -> String
+toCode trait = case mconcat $ getAlleles trait of
+    Allele Dominant -> codeToUpper $ representingCode trait
+    Allele Recessive -> codeToLower $ representingCode trait
+
+toPhenotype :: Genotype -> Phenotype
+toPhenotype traits = Phenotype (map toCode traits)
+
+fromGenotype :: Int -> Genotype -> Offspring
+fromGenotype n genes = Offspring genes (1.0/n')
+    where
+        n' = fromIntegral n
+
+fromGenotypes :: [Genotype] -> Generation
+fromGenotypes xs = map (fromGenotype (length xs)) xs
+
 
 -- NOTE: is it really working?
 combine :: Trait -> Trait -> [(Trait, ProbRatio)]
@@ -198,95 +265,6 @@ combine (Trait oneCode oneAlleles) (Trait otherCode otherAlleles) = result
             | otherwise = (other, one)
 
 
-toCode :: Trait -> String
-toCode trait = case mconcat $ getAlleles trait of
-    Allele Dominant -> codeToUpper $ representingCode trait
-    Allele Recessive -> codeToLower $ representingCode trait
-
-type Genotype = [Trait]
-
-toPhenotype :: Genotype -> Phenotype
-toPhenotype traits = Phenotype (map toCode traits)
-
-
-newtype Phenotype = Phenotype {getCodes :: [Code]}
-    deriving (Show)
-
-
-data Offspring = Offspring { getType :: Genotype, prob :: ProbRatio}
-    deriving (Eq)
-
-instance Ord Offspring where
-    compare (Offspring g1 _) (Offspring g2 _) = compare (sort g1) (sort g2)
-
-instance Show Offspring where
-    show (Offspring geno p) = "(Offspring: " ++ show geno ++ ", " ++ show p ++ ")"
-
-fromGenotype :: Int -> Genotype -> Offspring
-fromGenotype n genes = Offspring genes (1.0/n')
-    where
-        n' = fromIntegral n
-
-type Generation = [Offspring]
-
-fromGenotypes :: [Genotype] -> Generation
-fromGenotypes xs = map (fromGenotype (length xs)) xs
-
-
-
-nextGen :: Int -> Generation -> Generation
-nextGen bad current | bad <= 0 = current
-nextGen 1 current = new
-    where
-
-        -- Why probability is calculated so? 
-        -- Ask me in person if you want, I can't explain it here
-        probability xs 
-            | length xs <= 1 = fromIntegral (length xs)
-            | otherwise = 1 / fromIntegral (length xs * (length xs - 1))
-        
-        offspringsCombination = concat [combineOffsprings x y | x <- current, y <- current]
-        preprocessedCombination = map (\(Offspring g p) -> (g, p)) offspringsCombination
-        new = map (\(g, p) -> Offspring g (p * probability current)) (summingUp preprocessedCombination)
-nextGen n current = nextGen (n-1) (next current)
-
--- -- Что делать с ошибками?
-combineOffsprings :: Offspring  -> Offspring -> [Offspring]
-combineOffsprings one other = if one == other then [] else result
-    where
-        resultingGeno :: [(Genotype, ProbRatio)]
-        resultingGeno = combineGenotypes (getType one) (getType other)
-        summed = summingUp resultingGeno
-        result = map (uncurry Offspring) summed
-
-
-geno1 :: Genotype
-geno1 = [eyeSizeTrait [recessiveAllele, dominantAllele], furTrait [recessiveAllele, dominantAllele]]
-
-geno2 :: Genotype
-geno2 = [eyeSizeTrait [dominantAllele, dominantAllele], furTrait [dominantAllele, dominantAllele]]
-
-geno3 :: Genotype
-geno3 = [eyeSizeTrait [recessiveAllele, dominantAllele], furTrait [dominantAllele, dominantAllele]]
-
-geno4 :: Genotype
-geno4 = [eyeSizeTrait [dominantAllele, recessiveAllele], furTrait [dominantAllele,recessiveAllele]]
-
-
-summingUp :: (Ord a, Ord b, Num b) => [([a], b)] -> [([a], b)]
-summingUp xs = summed
-    where
-        sorted = sort $ map (\(l, v) -> (sort l, v)) xs
-        grouped = groupBy comparator sorted
-        comparator (l1, _) (l2, _) = l1 == l2
-
-        summed = concatMap getFirstValueFromListAndSumProbs grouped
-
-        -- | ha-ha
-        getFirstValueFromListAndSumProbs [] = []
-        getFirstValueFromListAndSumProbs ((x, currProb):xs') = [(x, currProb + sum (map snd xs'))]
-
-
 combineGenotypes :: Genotype -> Genotype -> [(Genotype, ProbRatio)]
 combineGenotypes first second = summingUp folded
     where
@@ -295,6 +273,54 @@ combineGenotypes first second = summingUp folded
         g (old, prevProb) (current, currProb) = (old ++ current, prevProb * currProb)
         castedOffsprings = map (map (\(x,y) -> ([x], y))) offsprings
         offsprings = filter (/= []) [combine traitX traitY | traitX <- first, traitY <- second]
+
+
+-- -- Что делать с ошибками?
+combineOffsprings :: Offspring  -> Offspring -> [Offspring]
+combineOffsprings one other = if one == other then [] else result
+    where
+        resultingGeno :: [(Genotype, ProbRatio)]
+        resultingGeno = combineGenotypes (getType one) (getType other)
+
+        oneRatio = prob one
+        otherRatio = prob other
+        offstringCoeff = (otherRatio / (1 - oneRatio)) * (oneRatio / (1 - otherRatio))
+
+        castedGeno = map f resultingGeno
+        f (geno, genoRatio) = (geno, genoRatio * offstringCoeff)
+
+        summed = summingUp castedGeno
+        result = map (uncurry Offspring) summed
+
+
+next :: Generation -> Generation
+next = nextGen 1
+
+summingUp :: (Ord a, Ord b, Num b) => [([a], b)] -> [([a], b)]
+summingUp xs = summed
+    where
+        sorted = sort $ map (\(l, v) -> (sort l, v)) xs
+        grouped = groupBy comparator sorted
+        comparator (l1, _) (l2, _) = l1 == l2
+
+        summed = concatMap compact grouped
+
+        -- | gets first value of the pair in the list and 
+        -- | sumes all second values with the same first value
+        compact [] = []
+        compact ((x, currProb):xs') = [(x, currProb + sum (map snd xs'))]
+
+nextGen :: Int -> Generation -> Generation
+nextGen bad current | bad <= 0 = current
+nextGen 1 current = new
+    where
+        -- DUPLICATE OFFSPRINGS HERE!!!!!!!!!!!!!! FUCK
+        -- FIXME:
+        -- [Offspring [Trair] ProbRatio]
+        offspringsCombination = concat [combineOffsprings x y | x <- current, y <- current]
+        preprocessedCombination = map (\(Offspring g p) -> (g, p)) offspringsCombination
+        new = map (\(g, p) -> Offspring g p) (summingUp preprocessedCombination)
+nextGen n current = nextGen (n-1) (next current)
 
 -- есть gen1, gen2 - одинаковые по длине и признаку? если признак есть только во втором?
 -- нужно combine каждый из 1 с каждым из второго N*N 
@@ -315,6 +341,8 @@ nextWithPedofil n pedofil current = nextWithPedofil (n-1) newPedofil newGen
         newGen = nextGen 1 updated
         updated = getUpdated pedofil current
         newPedofil = fmap getType (listToMaybe newGen)
+
+-- second version of combinations to be able to rollback to parents genotypes
 
 -- combine but without reduce and sort
 combine2 :: Trait -> Trait -> [(Trait, ProbRatio)]
@@ -356,5 +384,3 @@ combineOffsprings2 one other = if one == other then [] else result
         oneRatio = prob one
         otherRatio = prob other
 
-osp3 = Offspring [furTrait [dominantAllele, dominantAllele], eyeSizeTrait [recessiveAllele, recessiveAllele]] (1/3)
-osp4 = Offspring [furTrait [recessiveAllele, dominantAllele], eyeSizeTrait [recessiveAllele, dominantAllele]] (2/3)
